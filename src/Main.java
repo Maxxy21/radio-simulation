49be53edeec3c16dd28b41c1f2a04d0fc1e273cf
Maxwell Aboagye <nanabaah12@gmail.com>
2023-10-10 21:39:00 +0200

    Zusätzliche Funktionalitäten hinzugefügt

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,39 @@ public class Main {
     public static void main(String[] args) {
         Radio meinRadio = new Radio();
 
+        System.out.println("Anfänglicher Status:");
+        meinRadio.displayStatus();
+
+        System.out.println("\nRadio einschalten:");
         meinRadio.turnOn();
-        meinRadio.increaseVolume();
-        meinRadio.setFrequency(95.5);
+
+        System.out.println("\nLautstärke direkt auf 55 einstellen:");
+        meinRadio.setVolume(55);
+
+        System.out.println("\nWechseln Sie in den AM-Modus und stellen Sie die Frequenz auf 1000 kHz ein:");
+        meinRadio.switchToAM();
+        meinRadio.setFrequency(1050);
+
+        System.out.println("\nLautstärke verringern:");
         meinRadio.decreaseVolume();
+
+        System.out.println("\nRadio stummschalten:");
+        meinRadio.mute();
+
+        System.out.println("\nVersuchen Sie, die Lautstärke zu erhöhen, wenn sie gedämpft sind (die Dämpfung sollte aufgehoben werden):");
+        meinRadio.increaseVolume();
+
+        System.out.println("\nWechseln Sie zurück in den FM-Modus und stellen Sie die Frequenz auf 96,5 MHz ein:");
+        meinRadio.switchToFM();
+        meinRadio.setFrequency(96.5);
+
+        System.out.println("\nLautstärke verringern:");
+        meinRadio.decreaseVolume();
+
+        System.out.println("\nEndstatus:");
+        meinRadio.displayStatus();
+
+        System.out.println("\nRadio ausschalten:");
         meinRadio.turnOff();
     }
 }
--- src/Radio.java
+++ src/Radio.java
@@ -6,14 +6,23 @@
  */
 public class Radio {
 
-    private static final int MAX_VOLUME = 10;
+    private static final int MAX_VOLUME = 100;
     private static final int MIN_VOLUME = 0;
-    private static final double MIN_FREQUENCY = 88.0;
-    private static final double MAX_FREQUENCY = 108.0;
+    private static final double MIN_FREQUENCY_FM = 88.0;
+    private static final double MAX_FREQUENCY_FM = 108.0;
+    private static final double MIN_FREQUENCY_AM = 530.0;
+    private static final double MAX_FREQUENCY_AM = 1700.0;
+
 
     private boolean isOn;
     private int volume;
     private double frequency;
+    private boolean isMuted;
+    private Mode currentMode;
+
+    private enum Mode {
+        AM, FM
+    }
 
 
     /**
@@ -24,6 +33,8 @@ public class Radio {
         this.isOn = false;
         this.volume = 0;
         this.frequency = 88.0;
+        this.isMuted = false;
+        this.currentMode = Mode.FM; // FM ist der Standardmodus
     }
 
 
@@ -66,6 +77,9 @@ public class Radio {
      */
     public void increaseVolume() {
         ensureRadioIsOn();
+        if (isMuted) {
+            unmute();
+        }
         if (volume < MAX_VOLUME) {
             volume++;
             System.out.println("Lautstärke: " + volume);
@@ -78,9 +92,11 @@ public class Radio {
      * Verringert die Lautstärke um eine Einheit, wenn möglich.
      * Druckt relevante Meldungen basierend auf dem Zustand des Radios und der aktuellen Lautstärke.
      */
-
     public void decreaseVolume() {
         ensureRadioIsOn();
+        if (isMuted) {
+            unmute();
+        }
         if (volume > MIN_VOLUME) {
             volume--;
             System.out.println("Lautstärke: " + volume);
@@ -89,19 +105,99 @@ public class Radio {
         }
     }
 
+    /**
+     * Setzt die Lautstärke des Radios auf den angegebenen Wert.
+     *
+     * @param volume Die gewünschte Lautstärke, die zwischen {@link #MIN_VOLUME} und {@link #MAX_VOLUME} liegen sollte.
+     */
+    public void setVolume(int volume) {
+        ensureRadioIsOn();
+        if (volume < MIN_VOLUME || volume > MAX_VOLUME) {
+            System.out.println("Lautstärke muss zwischen " + MIN_VOLUME + " und " + MAX_VOLUME + " liegen");
+            return;
+        }
+        this.volume = volume;
+        System.out.println("Lautstärke eingestellt auf: " + volume);
+    }
+
 
     /**
      * Setzt die Frequenz des Radios auf den angegebenen Wert.
      *
-     * @param frequency Die gewünschte Frequenz, die zwischen {@link #MIN_FREQUENCY} und {@link #MAX_FREQUENCY}  liegen sollte.
+     * @param frequency Die gewünschte Frequenz, die zwischen MIN_FREQUENCY_FM und MAX_FREQUENCY liegen sollte.
      */
     public void setFrequency(double frequency) {
         ensureRadioIsOn();
-        if (frequency < MIN_FREQUENCY || frequency > MAX_FREQUENCY) {
-            System.out.println("Frequenz muss zwischen " + MIN_FREQUENCY + " und " + MAX_FREQUENCY + " liegen");
+
+        double minFreq = (currentMode == Mode.AM) ? MIN_FREQUENCY_AM : MIN_FREQUENCY_FM;
+        double maxFreq = (currentMode == Mode.AM) ? MAX_FREQUENCY_AM : MAX_FREQUENCY_FM;
+
+        if (frequency < minFreq || frequency > maxFreq) {
+            System.out.println("Frequenz muss zwischen " + minFreq + " und " + maxFreq + " liegen");
             return;
         }
         this.frequency = frequency;
-        System.out.println("Frequenz eingestellt auf: " + frequency + " MHz");
+
+        if (currentMode == Mode.AM) {
+            System.out.println("Frequenz eingestellt auf: " + frequency + " kHz");
+        } else {
+            System.out.println("Frequenz eingestellt auf: " + frequency + " MHz");
+        }
+    }
+
+    /**
+     * Macht das Radio stumm, wenn es nicht stumm ist.
+     */
+    public void mute() {
+        ensureRadioIsOn();
+        isMuted = true;
+        System.out.println("Lautstärke ist gedämpft");
+    }
+
+    /**
+     * Hebt die Stummschaltung des Radios auf, wenn es stumm ist.
+     */
+    public void unmute() {
+        ensureRadioIsOn();
+        isMuted = false;
+        System.out.println("Lautstärke ist ungedämpft");
+    }
+
+    /**
+     * Schaltet das Radio in den AM-Modus.
+     * Wenn das Radio bereits im AM-Modus ist, wird eine entsprechende Nachricht angezeigt.
+     */
+    public void switchToAM() {
+        ensureRadioIsOn();
+        currentMode = Mode.AM;
+        System.out.println("Umgeschaltet auf AM-Modus");
+    }
+
+    /**
+     * Schaltet das Radio in den FM-Modus.
+     * Wenn das Radio bereits im FM-Modus ist, wird eine entsprechende Nachricht angezeigt.
+     */
+    public void switchToFM() {
+        ensureRadioIsOn();
+        currentMode = Mode.FM;
+        System.out.println("Umgeschaltet auf FM-Modus");
+    }
+
+    /**
+     * Gibt den aktuellen Status des Radios aus.
+     */
+    public void displayStatus() {
+        System.out.println("Radio-Status: " + (isOn ? "AN" : "AUS"));
+        System.out.println("Modus: " + currentMode);
+        System.out.println("Lautstärke: " + volume);
+        System.out.println("Gedämpft: " + (isMuted ? "Ja" : "Nein"));
+        if (isOn) {
+            if (currentMode == Mode.AM) {
+                System.out.println("Frequenz: " + frequency + " kHz");
+            } else if (currentMode == Mode.FM) {
+                System.out.println("Frequenz: " + frequency + " MHz");
+            }
+        }
     }
 }
+
